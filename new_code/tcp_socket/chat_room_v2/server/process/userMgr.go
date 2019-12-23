1547d9142d032f2d7a36a55ee02b369fab4000d4
zhenlongl <742946057@qq.com>
2019-12-23 21:37:48 +0800

    userMgr.go 操作在线用户相关

--- new_code/tcp_socket/chat_room_v2/server/process/userMgr.go
+++ new_code/tcp_socket/chat_room_v2/server/process/userMgr.go
@@ -1,5 +1,50 @@
-package main
+package process
 
-func main() {
-	$END$
+import "fmt"
+
+// 因为userMgr实例在服务器端有且只有一个
+// 因为在很多地方都会使用他，因为把他做成全局变量
+
+var userMgr *UserMgr
+
+type UserMgr struct {
+	onlineUsers map[int]*UserProcess
+}
+
+
+//初始化userMgr
+func init()  {
+	userMgr = &UserMgr{
+		onlineUsers:make(map[int]*UserProcess,1024),
+	}
+}
+
+// 添加在线用户
+func (UserMgr *UserMgr)AddOnlineUser(userProcess *UserProcess) {
+	UserMgr.onlineUsers[userProcess.UserId] = userProcess
+}
+
+func (UserMgr *UserMgr)DeleteOnlineUser(userProcess *UserProcess)  {
+
+	// 判断该用户是否在线，如果在线将他从map删除
+	if _,ok := UserMgr.onlineUsers[userProcess.UserId];ok {
+		delete(UserMgr.onlineUsers, userProcess.UserId)
+	}
+
+}
+
+// 获取当前所有在线用户
+func (UserMgr *UserMgr)GetAllOnlineUsers() map[int]*UserProcess  {
+	return UserMgr.onlineUsers
+}
+
+func (UserMgr *UserMgr)GetOnlineUserById(userId int)(userProcess *UserProcess ,err error) {
+	userProcess, ok := UserMgr.onlineUsers[userId]
+	if !ok {
+		err = fmt.Errorf("当前用户%d,不在线",userId)
+		goto END
+	}
+	
+	END:
+	return
 }
--- statisticsSys/hello/main.go
+++ statisticsSys/hello/main.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
+func test(c chan int)  {
+	for i:=0;i<100;i++ {
+		if i % 10 == 0 {
+			fmt.Println(i)
+		}
+	}
+	c <- 1
+}
+func test1()  {
+	for i:=0;i<100;i++ {
+		if i % 10 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
 func main() {
-	$END$
+	//message := make(chan string)
+	c := make(chan int)
+	/*go func() {
+		message <- "hello goroutine"
+		//fmt.Println("hello goroutine")
+	}()*/
+
+	//go test1()
+	go test(c)
+	//fmt.Println(<- message)
+	fmt.Println("hello world")
+	fmt.Println(<-c)
 }
